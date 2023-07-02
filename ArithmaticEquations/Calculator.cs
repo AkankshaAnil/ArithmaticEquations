41684ed9d14c3bdb8a54bd1022921d6257c29f54
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-02 22:50:46 +0530

    [AkankshaAnil] ArithmaticEquations

---
+++ ArithmaticEquations/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmaticEquations
+{
+    internal class Calculator
+    {
+        public void AdditionOfTwoNumbers()
+        {
+            Console.WriteLine("Please provide 2 numbers for addition");
+            Console.WriteLine("Please enter 1st number");
+            int firstNumb = Convert.ToInt32(Console.ReadLine()); //"10" 10
+            Console.WriteLine("Please enter 2nd number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum:" + (firstNumb + secondNumber));
+        }
+
+        public void DifferenceOfTwoNumbers()
+        {
+            Console.WriteLine("Please provide 2 numbers for Substarction");
+            Console.WriteLine("Please enter 1st number");
+            int firstNumb = Convert.ToInt32(Console.ReadLine()); //"10" 10
+            Console.WriteLine("Please enter 2nd number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Diff:" + Math.Abs(firstNumb - secondNumber)); //10-20=-10
+        }
+    }
+}
+
---
+++ ArithmaticEquations/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ArithmaticEquations
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello All Welcome to fellowship");
+            //object creation of any class
+            Calculator calculator = new Calculator();
+            calculator.AdditionOfTwoNumbers();
+            calculator.DifferenceOfTwoNumbers();
+
+        }
+    }
+}
